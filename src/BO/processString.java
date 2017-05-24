63c5aadc6bdf9531506f8a0e34e63e7c28e866b3
Nam Anh Nguyen <Nam Anh Nguyen>
2017-05-24 22:40:36 +0700

    update string compararison case Almost-Full

--- src/BO/processString.java
+++ src/BO/processString.java
@@ -5,23 +5,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class processString {
-	File fileKey;
 	HashMap<String, String> key = new HashMap<>();
 
 	public String getAnswer(String imageText) {
-		key.put("which of the following does a policy change control board do?", "A and D");
-		String[] imageSplit = imageText.split("\n");
-		for (Map.Entry<String, String> entry : key.entrySet()) {
-			if (entry.getKey().trim().toLowerCase().equals(imageSplit[0].trim().toLowerCase())) {
-				for (int i = 1; i < imageSplit.length; i++) {
-					if (imageSplit[i].trim().toLowerCase().contains(entry.getValue().trim().toLowerCase())) {
-						return imageSplit[i].substring(0, 1);
+		int countDuplicateWord = 0;
+        String[] imageSplit = imageText.split("\n");
+        for (Map.Entry<String, String> entry : key.entrySet()) {
+            if (entry.getKey().trim().toLowerCase().equals(imageSplit[0].trim().toLowerCase())) {
+                for (int i = 1; i < imageSplit.length; i++) {
+                    if (imageSplit[i].trim().toLowerCase().contains(entry.getValue().trim().toLowerCase())) {
+                        return imageSplit[i].substring(0, 1);
+                    } else {
+						StringTokenizer st = new StringTokenizer(entry.getValue().trim().toLowerCase()); 
+						while(st.hasMoreTokens()) {
+						    if(imageSplit[i].trim().toLowerCase().contains(st.nextToken())) {
+							    countDuplicateWord++;
+							}
+						}
+						if(countDuplicateWord > 5) {
+							return imageSplit[i].substring(0, 1);
+						}
 					}
-				}
-			}
-		}
-		return "Z";
-	}
+                }
+            }
+        }
+        return "Z";
+    
 
 	public static void main(String[] args) {
 		processString p = new processString();

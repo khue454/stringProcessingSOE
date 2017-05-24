92ac8f4c4cb1452855a9630adbba57fd7bf8cfde
MrStar1102 <cutruong.tn92@gmail.com>
2017-05-24 15:52:05 +0700

    update test case
    update input string va map

--- src/BO/processString.java
+++ src/BO/processString.java
@@ -9,6 +9,7 @@ public class processString {
 	HashMap<String, String> key = new HashMap<>();
 
 	public String getAnswer(String imageText) {
+		key.put("which of the following does a policy change control board do?", "A and D");
 		String[] imageSplit = imageText.split("\n");
 		for (Map.Entry<String, String> entry : key.entrySet()) {
 			if (entry.getKey().trim().toLowerCase().equals(imageSplit[0].trim().toLowerCase())) {
@@ -20,5 +21,11 @@ public class processString {
 			}
 		}
 		return "Z";
+	}
+
+	public static void main(String[] args) {
+		processString p = new processString();
+		String a = "—\nWhich of the following does a policy change control board do?\n\nA. Assesses policies and standards and makes recommendations for change\n\nB. Determines the policy and standards library numbering scheme\n\nC. Implements technical controls as business conditions change\n\nD. Reviews requested changes to the policy framework\n\nE. A and D\n\n";
+		p.getAnswer(a);
 	}
 }
